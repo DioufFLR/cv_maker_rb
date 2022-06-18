349e3833b00b791db721c16e0274dc2bfbb2b2e5
DioufFLR <geoffreyfleur@gmail.com>
2022-06-18 17:55:38 +0200

    Heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module CvMaker
   class Application < Rails::Application
+
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.autoload_paths << "#{Rails.root}/lib"
